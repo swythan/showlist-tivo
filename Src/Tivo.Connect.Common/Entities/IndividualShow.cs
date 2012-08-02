e6510d978f180b5420076c7078555818f2830bae
James Chaldecott <james@swythan.co.uk>
2012-08-02 22:10:50 +0000

    Add recorded on date for shows in main list. Fix rendering in Light theme.
    git-tfs-id: [https://dev.azure.com/swythan]$/SwythanTivoProject/Dev/Dev;C116

--- Src/Tivo.Connect.Common/Entities/IndividualShow.cs
+++ Src/Tivo.Connect.Common/Entities/IndividualShow.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Globalization;
+using System.Diagnostics;
 
 namespace Tivo.Connect.Entities
 {
@@ -12,6 +14,7 @@ namespace Tivo.Connect.Entities
         }
 
         public string ContentId { get; set; }
+        public DateTime StartTime { get; set; }
 
         protected override void SetupFromRecordingFolderItemJson(IDictionary<string, object> jsonSource)
         {
@@ -19,6 +22,23 @@ namespace Tivo.Connect.Entities
 
             this.Id = (string)jsonSource["childRecordingId"];
             this.ContentId = (string)jsonSource["contentId"];
+
+            if (jsonSource.ContainsKey("startTime"))
+            {
+                var startTimeString = (string)jsonSource["startTime"];
+                DateTime startTime;
+                if (DateTime.TryParse(startTimeString, CultureInfo.InvariantCulture, DateTimeStyles.None, out startTime))
+                {
+                    this.StartTime = startTime;
+                }
+                else
+                {
+                    Debug.WriteLine(
+                        "Failed to parse startTime: {0}",
+                        jsonSource["startTime"]);
+                }
+            }
         }
+
     }
 }
