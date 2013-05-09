28b55bd38e40b90dff1aaea8bad4e28b9d73bab3
James Chaldecott <james@swythan.co.uk>
2013-05-09 22:57:48 +0000

    Update copyright & version numbers
    git-tfs-id: [https://dev.azure.com/swythan]$/SwythanTivoProject/Release/2.0;C217

--- Src/TivoAhoy.Phone/Properties/AssemblyInfo.cs
+++ Src/TivoAhoy.Phone/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Show List for TiVo")]
-[assembly: AssemblyCopyright("Copyright © James Chaldecott 2010")]
+[assembly: AssemblyCopyright("Copyright © James Chaldecott 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: NeutralResourcesLanguageAttribute("en")]
