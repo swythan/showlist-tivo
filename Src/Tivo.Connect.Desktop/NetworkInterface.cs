c4a7b642f5d9b5551adf2adf0b428c80e970c9e3
Oren Novotny <oren@novotny.org>
2013-09-30 18:40:18 +0000

    Switch desktop TLS impl to use BC
    git-tfs-id: [https://dev.azure.com/swythan]$/SwythanTivoProject/Dev/Portable;C304

--- Src/Tivo.Connect.Desktop/NetworkInterface.cs
+++ Src/Tivo.Connect.Desktop/NetworkInterface.cs
@@ -2,73 +2,52 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Security;
 using System.Net.Sockets;
-using System.Security.Authentication;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto.Tls;
 
 namespace Tivo.Connect.Platform
 {
     public class NetworkInterface : INetworkInterface
     {
         private TcpClient client;
-        private SslStream stream;
+        private TlsProtocolHandler tlsProtocolHandler;
 
         public async Task<Stream> Initialize(TivoEndPoint endPoint)
         {
-            if (this.client != null && this.client.Connected)
+            if (this.client != null)
             {
                 throw new InvalidOperationException("Cannot call Initialize on an already connected interface");
             }
-
-            if (this.client != null && !this.client.Connected)
-            {
-                this.stream.Dispose();
-                this.client.Close();
-            }
-
-            byte[] certBytes;
-            using (var ms = new MemoryStream())
-            {
-                await endPoint.Certificate.CopyToAsync(ms).ConfigureAwait(false);
-                certBytes = ms.ToArray();
-            }
-
-            var cert = new X509Certificate2(certBytes, endPoint.Password);
 
             // Create new connection
             this.client = new TcpClient();
 
             await this.client.ConnectAsync(endPoint.Address, (int)endPoint.Mode).ConfigureAwait(false);
 
-            this.stream = new SslStream(
-                this.client.GetStream(),
-                true,
-                // close inner stream on dispose
-                (sender, certificate, chain, errors) => true,
-                // ignore cert errors
-                (sender, host, certificates, certificate, issuers) => cert); // provide local cert
+            var tivoTlsClient = new TivoTlsClient(endPoint.Certificate, endPoint.Password);
 
-            this.stream.WriteTimeout = 10000;
+            this.tlsProtocolHandler = new TlsProtocolHandler(this.client.GetStream());
+            this.tlsProtocolHandler.Connect(tivoTlsClient);
 
-            await this.stream.AuthenticateAsClientAsync("Unused", new X509Certificate2Collection(), SslProtocols.Tls, false).ConfigureAwait(false);
-
-            return this.stream;
+            return this.tlsProtocolHandler.Stream;
         }
 
         public void Dispose()
         {
             try
             {
-                if (this.client.Connected)
+                if (this.client != null)
                 {
                     this.client.Close();
-                    this.stream.Dispose();
-
                     this.client = null;
-                    this.stream = null;
+                }
+
+                if (this.tlsProtocolHandler != null)
+                {
+                    this.tlsProtocolHandler.Close();
+                    this.tlsProtocolHandler = null;
                 }
             }
             catch (Exception)
